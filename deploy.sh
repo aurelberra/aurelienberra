f48eeb47db04172c0c2d812f7ce19b7735f3d241
Aurélien Berra <aurelien.berra@gmail.com>
2017-01-09 16:44:19 +0100

    Rebuilding site 2017-01-09 16:44:19

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,10 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 rm -r public
 git rm -r public
-rm -rf .git/modules/public
-git submodule add git@github.com:aurelberra/aurelberra.github.io.git public
+# rm -rf <submodule> && 
+git submodule update
+# rm -rf .git/modules/public
+# git submodule add git@github.com:aurelberra/aurelberra.github.io.git public
 git add .
 git push -u origin master
 
