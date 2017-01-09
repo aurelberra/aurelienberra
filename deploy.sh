866881c786681482c35a488eff98bd0d334cc8aa
Aurélien Berra <aurelien.berra@gmail.com>
2017-01-09 14:56:28 +0100

    rebuilding site Mon Jan  9 14:56:28 CET 2017

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project
+hugo
+
+# Add changes to git
+git add -A
+
+# Commit changes with provided argument or with date as message
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source
+git push origin master
