ca05d9fc213f45fae1e081ec752ded5548e99c3d
Aurélien Berra <aurelien.berra@gmail.com>
2017-01-09 20:46:46 +0100

    Improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,28 +2,7 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# rm -r public
-# git rm -r public
-# rm -rf <submodule> && git submodule update
-# rm -rf .git/modules/public
-# git submodule add git@github.com:aurelberra/aurelberra.github.io.git public
-
-git add .
-msg="Regenerate site `date +%Y-%m-%d\ %H:%M:%S`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-git push -u origin master
-
-# Build the project
-# hugo
-hugo -d /Users/aurel/Documents/github/aurelberra.github.io
-cd /Users/aurel/Documents/github/aurelberra.github.io # build public into github pages repo
-
-# Add changes to git
-# git add -A
-git add . # add all
+git add -A # better than "git add ." according to man page
 
 # Commit changes with provided argument or with date as message
 msg="Regenerate site `date +%Y-%m-%d\ %H:%M:%S`"
@@ -32,5 +11,20 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 
-# Push source
+git push -u origin master
+
+# Build the project
+# build public folder in github pages repo worked for me
+# hugo
+hugo -d /Users/aurel/Documents/github/aurelberra.github.io
+cd /Users/aurel/Documents/github/aurelberra.github.io
+
+git add -A
+
+msg="Regenerate site `date +%Y-%m-%d\ %H:%M:%S`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
 git push -u origin master
